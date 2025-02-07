1a989162f8e07160de4605b1bfe3516cc2989cf0
Natalia <nstanskaia@gmail.com>
2025-02-06 19:05:35 -0500

    add some example

--- src/com/neotech/lesson04/NewClass.java
+++ src/com/neotech/lesson04/NewClass.java
@@ -3,9 +3,16 @@ package com.neotech.lesson04;
 public class NewClass {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("New chnages by Khurliman");
-		
+
+		int a = 100;
+		int b = 200;
+		if (a > b) { // if true do this
+			System.out.print("a is greater than b");
+		} else { // if false do this
+			System.out.print("a is less than b");
+		}
 
 	}
 
